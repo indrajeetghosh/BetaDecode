5e1acaf2b32946213c97a117cd2967d4acf35c71
Indrajeet <oz35975@umbc.edu>
2024-05-08 11:57:59 -0400

    Update loss.py

--- loss.py
+++ loss.py
@@ -2,8 +2,8 @@ from torch.nn.functional import cosine_similarity
 from torch.nn import functional as F
 
 def kl_loss(mu, logvar):
-    KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp(), dim=1)
-    return KLD.sum()
+    KLD = torch.mean(-0.5 * torch.sum(1 + logvar - mu ** 2 - logvar.exp(), dim=1), dim=0)
+    return KLD
 
 def reconstruction_loss(x_reconstructed, x):
     cos_sim_loss = 1 - cosine_similarity(x, x_reconstructed, dim=1).mean()
